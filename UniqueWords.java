c5dc8b6ce265a51c80c1d6eab338f2bb332bd4aa
Oleg Kuleshov <106450501+Ahilles84@users.noreply.github.com>
2022-09-17 16:04:41 +0300

    Update UniqueWords.java
    forgot return

--- UniqueWords.java
+++ UniqueWords.java
@@ -2,18 +2,19 @@ public class UniqueWords {
 	public static int uniqueWordsCounter (String sourceText) {
 		String aligned = sourceText.toLowerCase;
 		String[] words = aligned.split("[.,?!]? +");
-		int uniqueCount = 0;
+		int uniqueCounter = 0;
 		for(int i = 0; i < words.length; i++){
-			int hitCount = 0;
+			int hitCounter = 0;
 			for(int j = 0; j < words.length; j++){
 				if(words.compareTo(words[i], words[j]){
-					hitCount++;
+					hitCounter++;
 				}
 			}
-			if(hitCount == 1){
-				uniqueCount++;
+			if(hitCounter == 1){
+				uniqueCounter++;
 			}
 		}
+		return uniqueCounter;
 	}
 	public static void main (String[] args){
 		String text = "Listen to the news from today and read the text at the same time. Listen to the news from today without reading the text.";
